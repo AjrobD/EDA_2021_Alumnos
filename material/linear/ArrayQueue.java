d4c68444b6010ca52a2e188f69cf18b0b439ac4c
AjrobD <borja_dosuna@hotmail.es>
2020-10-15 09:22:13 +0200

    ArrayQueue y LinkedQueue

--- material/linear/ArrayQueue.java
+++ material/linear/ArrayQueue.java
@@ -1,6 +1,68 @@
 package material.linear;
 
-public class ArrayQueue<E> {
+import material.Position;
 
+public class ArrayQueue<E>{
 
+    private class Node<E> implements Position<E> {
+        private E element;
+        public E getElement() {
+            return element;
+        }
+    }
+
+    private Node<E>[] queue;
+    private int top;
+    private int n;
+    private int MAX_SIZE;
+
+    public ArrayQueue(int size){
+        MAX_SIZE = size;
+        top = 0;
+        n=0;
+        queue = (Node<E>[]) new Node[size];
+    }
+
+    public int size(){
+        return n;
+    }
+
+    public boolean isEmpty(){
+        return n==0;
+    }
+
+    public E front() throws RuntimeException{
+        if (isEmpty()){
+            throw new RuntimeException("This queue is empty");
+        }
+        return queue[top].getElement();
+    }
+
+    public void enqueue(E element) {
+        Node<E> nodo = (Node<E>) element;
+        if(n<MAX_SIZE){
+            queue[(top+n)%MAX_SIZE] = nodo;
+        }
+        else{
+            Node<E>[] queue2 = (Node<E>[]) new Node[MAX_SIZE*2];
+            for (int i = 0; i < MAX_SIZE; i++){
+                queue2[i] = queue[(top+i)%MAX_SIZE];
+            }
+            queue2[MAX_SIZE] = nodo;
+            queue = queue2;
+            top = 0;
+            n++;
+            MAX_SIZE=MAX_SIZE*2;
+        }
+    }
+
+    public E dequeue() throws RuntimeException{
+        if (isEmpty()){
+            throw new RuntimeException("This queue is empty");
+        }
+        E elemento = queue[top].getElement();
+        top=(top+1)%MAX_SIZE;
+        n--;
+        return elemento;
+    }
 }
--- material/linear/LinkedQueue.java
+++ material/linear/LinkedQueue.java
@@ -1,5 +1,78 @@
 package material.linear;
 
+import material.Position;
+import sun.awt.image.ImageWatched;
+
 public class LinkedQueue<E> {
+    private class DNode<E> implements Position<E> {
+
+        private DNode<E>  next;
+        private E element;
+
+        /**
+         * Constructor
+         */
+        public DNode(DNode<E> newNext, E elem) {
+            next = newNext;
+            element = elem;
+        }
+
+        @Override
+        public E getElement() {
+            return element;
+        }
+
+        public DNode<E> getNext() {
+            return next;
+        }
+
+        public void setNext(DNode<E> newNext) {
+            next = newNext;
+        }
+
+        public void setElement(E newElement) {
+            element = newElement;
+        }
+    }
+
+    private int size;
+    private DNode<E> head,tail;
+
+    public LinkedQueue(){
+        size=0;
+        head=null;
+        tail=null;
+    }
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public E front() throws RuntimeException{
+        if(isEmpty()){
+            throw new RuntimeException("This queue is empty");
+        }
+        return head.getElement();
+    }
+
+    public void enqueue(E element){
+        DNode<E> nodo = new DNode<E>(null,element);
+        tail.setNext(nodo);
+        tail = nodo;
+        size++;
+    }
+
+    public DNode<E> dequeue() throws RuntimeException{
+        if(isEmpty()){
+            throw new RuntimeException("This queue is empty");
+        }
+        DNode<E> nodo = head;
+        nodo.setNext(null);
+        head = head.getNext();
+        size--;
+        return nodo;
+    }
 
 }
